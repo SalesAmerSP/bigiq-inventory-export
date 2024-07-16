9a90b8b269cc35b143bd8fe05f96a73ea93e0c4b
Greg Robinson <g.robinson@f5.com>
2024-07-16 15:42:31 -0400

    Adding pandas

--- bigiq-inventory-export.py
+++ bigiq-inventory-export.py
@@ -6,6 +6,7 @@ import json
 import logging
 import argparse
 import sys
+import pandas
 
 logging.basicConfig(level=logging.INFO,filename='bigiq-inventory-export.log')
 logger = logging.getLogger(__name__)
@@ -122,14 +123,32 @@ def parse_arguments():
     parser.add_argument("--password", type=str, required=True, help="password for BIG-IQ user")
     parser.add_argument("--hostname", type=str, required=True, help="BIG-IQ host (IP/FQDN)")
     parser.add_argument("--csv", type=str, required=False, help="CSV to write output")
-    parser.add_argument("--virtual-servers", action="store_true", required=False, help="CSV to write output")
-    parser.add_argument("--pools", action="store_true", required=False, help="CSV to write output")
-    parser.add_argument("--pool-members", action="store_true", required=False, help="CSV to write output")
     parser.add_argument("--debug", action="store_true")
 
     # Parse arguments
     args = parser.parse_args()
     return args
+
+def Retrieve_Virtual_Servers():
+    virtual_servers = bigiq_http_get(
+        uri='mgmt/cm/adc-core/working-config/ltm/virtual',
+        params=''
+    )
+    return virtual_servers.json()
+
+def Retrieve_Pools():
+    pools = bigiq_http_get(
+        uri='mgmt/cm/adc-core/working-config/ltm/pool',
+        params=''
+    )
+    return pools.json()
+
+def Retrieve_Pool_Members():
+    pool_members = bigiq_http_post(
+        uri='mgmt/shared/pipeline/manager/All-Pool-Members-Pipeline',
+        payload='{"multiStageQueryRequest":{"repeatLastStageUntilTerminated":false,"queryParamsList":[{"description":"retrieval","filterProcessorReference":{"link":"https://localhost/mgmt/shared/index/es-config?%24filter=kind%20eq%20\'cm%3Aadc-core%3Aworking-config%3Altm%3Apool%3Amembers%3Aadcpoolmemberstate\'&%24orderby=name%20asc&%24top=150&%24skip=0"},"pipelineAction":"DATA_RETRIEVAL","runStageInternally":false},{"description":"expand","managedPipelineWorkerName":"expand-pipe","jsonContext":{"references":[{"expand":"parentInfo/deviceReference","select":["hostname","properties"]}]}},{"description":"stats","managedPipelineWorkerName":"resource-stats-pipe","pipelineAction":"DATA_PROCESSING","runStageInternally":false}]},"getOnPostAndTerminate":true,"isPerformanceBoostingEnabled":false}'
+    )
+    return pool_members
 
 def main():
     # Define BIG-IQ environment variables
@@ -144,7 +163,11 @@ def main():
     username = args.username
     password = args.password
     host = args.hostname
-
+    virtual_servers = Retrieve_Virtual_Servers()
+    pools = Retrieve_Pools()
+    pool_members = Retrieve_Pool_Members()
+    for current_virtual_server in virtual_servers['items']:
+      print(current_virtual_server)
 
 if __name__ == '__main__':
     main()
